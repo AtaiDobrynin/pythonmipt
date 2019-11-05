1681253ecac2b93ac2d00f86d6d3bbe9273aa04b
AtaiDobrynin <atay.dobrynin@phystech.edu>
2019-11-05 11:06:54 +0300

    Started course 2

---
+++ course2/adapter.py
@@ -0,0 +1,19 @@
+class MappingAdapter:
+	def __init__(self, adaptee):
+		self.adaptee = adaptee
+
+	def lighten(self, grid):
+		self.adaptee.set_dim((len(grid[0]), len(grid)))
+		lights = list()
+		obstacles = list()
+		for i in range(self.adaptee.dim[0]):
+			for j in range(self.adaptee.dim[1]):
+				if grid[j][i] == 1:
+					lights.append((i, j))
+				elif grid[j][i] == -1:
+					obstacles.append((i, j))
+		self.adaptee.set_lights(lights)
+		self.adaptee.set_obstacles(obstacles)
+		return self.adaptee.generate_lights()
+		
+		
---
+++ course2/check_adapter.py
@@ -0,0 +1,40 @@
+from adapter import MappingAdapter
+
+
+class Light:
+	def __init__(self, dim):
+		self.dim = dim
+		self.grid = [[0 for i in range(dim[0])] for _ in range(dim[1])]
+		self.lights = []
+		self.obstacles = []
+    
+	def set_dim(self, dim):
+		self.dim = dim
+		self.grid = [[0 for i in range(dim[0])] for _ in range(dim[1])]
+
+	def set_lights(self, lights):
+		self.lights = lights
+		self.generate_lights()
+
+	def set_obstacles(self, obstacles):
+		self.obstacles = obstacles
+		self.generate_lights()
+		
+	def generate_lights(self):
+		return self.grid.copy()
+		
+
+class System:
+	def __init__(self):
+		self.chart = self.grid = [[0 for i in range(30)] for _ in range(20)]
+		self.chart[5][7] = 1 # Источники света
+		self.chart[5][2] = -1 # Стены
+	
+	def get_lightening(self, light_chartper):
+		self.lightchart = light_chartper.lighten(self.chart)
+
+		
+system = System()
+lighter1 = Light((1 ,1))
+adapter1 = MappingAdapter(lighter1)
+system.get_lightening(adapter1)
